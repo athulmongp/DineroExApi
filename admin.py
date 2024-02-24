c7bac764bc526b2274d01de27e3311ee9e4e2aa0
athulmongp <athulmon1928@gmail.com>
2024-02-24 16:55:04 +0530

    f2

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from Finnest_app.models import *
-
-admin.site.register(Personn)
-# admin.site.register(CommonFields)
-admin.site.register(ModuleList)
-admin.site.register(ModulePermission)
