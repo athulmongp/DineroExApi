515ce1af14fe895e248b2f63f69c415198e6700e
athulmongp <141259190+athulmongp@users.noreply.github.com>
2024-02-24 11:27:55 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from Finnest_app.models import *
+
+admin.site.register(Personn)
+# admin.site.register(CommonFields)
+admin.site.register(ModuleList)
+admin.site.register(ModulePermission)
---
+++ serializer.py
@@ -0,0 +1,47 @@
+from .models import *
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+class Person_Details(serializers.ModelSerializer):
+    class Meta:
+        model  = User
+        fields = '__all__'
+
+class CustomLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        # Authenticate user
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("Invalid username or password.")
+
+        if not user.is_active:
+            raise serializers.ValidationError("User is not active.")
+
+        data['user'] = user
+        return data 
+    
+# class ModuleListSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ModuleList
+#         fields = '__all__'      
+    
+class ModuleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModuleList
+        fields = ['id','name']       
+
+class ModulePermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModulePermission
+        fields = [ 'moduleid','userid','canAccess','canCreate','canEdit','canDelete']
+        
+        
+        
