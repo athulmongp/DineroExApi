c3ab35e7215e45f575d6d80249f4edb2105d354b
athulmongp <athulmon1928@gmail.com>
2024-02-24 12:12:50 +0530

    project login

---
+++ Finnets_project/Finnest_app/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    
+    path('',views.deatils.as_view()),
+    path('Login/',views.CustomLoginView.as_view()),
+    path('person2/<int:pk>',views.deatils2.as_view()) ,
+
+    # ----moduleList-----
+    path('moduleListCreation',views.ModuleListCreationview.as_view()) ,
+    path('moduleListGet',views.ModuleListGetview.as_view()) ,
+    path('moduleListEdit/<int:pk>',views.ModuleListEditView.as_view()) ,
+    path('moduleListDelete/<int:pk>',views.ModuleListDeleteView.as_view()) ,
+
+    # ----moduleListPermission-----
+
+    path('moduleListtPermissionCreation',views.ModuleListPermissionCreationview.as_view()) ,
+    path('moduleListtPermissionGet',views.ModuleListPermissionGetview.as_view()) ,
+    path('moduleListPermissionEdit/<int:pk>',views.ModuleListPermissionEditView.as_view()) ,
+    path('moduleListPermissionDelete/<int:pk>',views.ModuleListPermissionDeleteView.as_view()) ,
+]
---
+++ Finnets_project/Finnest_app/views.py
@@ -0,0 +1,287 @@
+from django.shortcuts import render
+from .models import *
+from rest_framework import generics,permissions
+from rest_framework .views import APIView
+from rest_framework.response import Response
+from .serializer import *
+from knox.auth import TokenAuthentication
+from knox.views import LoginView as KnoxLoginView
+from knox.models import AuthToken
+from rest_framework import status
+from django.contrib.auth.models import User
+from  datetime import date
+from django.http import HttpRequest, HttpResponse
+
+
+def  Homepage(request):
+    
+    return render(request,'home.html')
+
+class deatils(generics.ListAPIView): 
+    queryset = User.objects.all() 
+    serializer_class = Person_Details
+
+class deatils2(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Personn.objects.all()
+    serializer_class = Person_Details
+class CustomLoginView(KnoxLoginView):
+    serializer_class = CustomLoginSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data.get('user', None)
+
+        if user is not None and user.is_active:
+            _, token = AuthToken.objects.create(user)
+            return Response({'auth_token': token}, status=status.HTTP_200_OK)
+        else:
+            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED) 
+        
+
+
+         
+
+# ----moduleList-----
+        
+class ModuleListCreationview(generics.CreateAPIView):
+    queryset = ModuleList.objects.all() 
+    serializer_class = ModuleListSerializer(queryset, many=True)
+    
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = ModuleListSerializer(data=request.data)
+        if  ModuleList.objects.filter(**request.data).exists():
+            raise serializers.ValidationError('This is Already Exists')
+        if serializer.is_valid():
+            serializer.validated_data['createdBy'] = self.request.user.username
+            serializer.validated_data['createdDate'] = date.today()
+            serializer.validated_data['lastMoodifiedBy'] = self.request.user.username
+            serializer.validated_data['lastMoodifiedDate'] = date.today()
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+
+class ModuleListGetview(generics.ListAPIView):
+    queryset = ModuleList.objects.all()
+    serializer_class = ModuleListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+   
+    
+    def get(self, request, *args, **kwargs):
+        
+        if not request.user.is_authenticated:
+            return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
+        response = super().get(request, *args, **kwargs)
+        return response
+
+
+class ModuleListEditView(generics.UpdateAPIView):
+    queryset = ModuleList.objects.all()
+    serializer_class = ModuleListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request, pk, format=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+         
+        if  ModuleList.objects.filter(**request.data).exists():
+            raise serializers.ValidationError('This is Already Exists')
+        if serializer.is_valid():
+            serializer.validated_data['lastMoodifiedBy'] = self.request.user.username
+            serializer.validated_data['lastMoodifiedDate'] = date.today()
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+class ModuleListDeleteView(generics.DestroyAPIView):
+    queryset = ModuleList.objects.all()
+    serializer_class = ModuleListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, pk, format=None):
+        instance = self.get_object()
+        
+        instance.delete()
+        message = 'ModuleList with id {} has been deleted'.format(pk)
+        return Response({'message': message}, status=status.HTTP_204_NO_CONTENT)
+    
+
+
+# ----moduleListPermission-----
+    
+class ModuleListPermissionCreationview(generics.CreateAPIView):
+    queryset = ModulePermission.objects.all() 
+    serializer_class = ModulePermissionSerializer(queryset, many=True)
+    
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = ModulePermissionSerializer(data=request.data)
+        if  ModulePermission.objects.filter(**request.data).exists():
+            raise serializers.ValidationError('This is Already Exists')
+        if serializer.is_valid():
+            serializer.validated_data['createdBy'] = self.request.user.username
+            serializer.validated_data['createdDate'] = date.today()
+            serializer.validated_data['lastMoodifiedBy'] = self.request.user.username
+            serializer.validated_data['lastMoodifiedDate'] = date.today()
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+class ModuleListPermissionGetview(generics.ListAPIView):
+    queryset = ModulePermission.objects.all() 
+    serializer_class = ModulePermissionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
+        response = super().get(request, *args, **kwargs)
+        return response
+
+
+class ModuleListPermissionEditView(generics.UpdateAPIView):
+    queryset = ModulePermission.objects.all() 
+    serializer_class = ModulePermissionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request, pk, format=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+         
+        if  ModulePermission.objects.filter(**request.data).exists():
+            raise serializers.ValidationError('This is Already Exists')
+        if serializer.is_valid():
+            serializer.validated_data['lastMoodifiedBy'] = self.request.user.username
+            serializer.validated_data['lastMoodifiedDate'] = date.today()
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+    
+
+class ModuleListPermissionDeleteView(generics.DestroyAPIView):
+    queryset = ModulePermission.objects.all()
+    serializer_class = ModulePermissionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, pk, format=None):
+        instance = self.get_object()
+        
+        instance.delete()
+        message = 'ModuleListPermission with id {} has been deleted'.format(pk)
+        return Response({'message': message}, status=status.HTTP_204_NO_CONTENT)
+
+    
+
+
+# class ModuleListGetview(generics.ListAPIView):
+#     queryset = ModuleList.objects.all() 
+#     serializer_class = ModuleListSerializer
+#     permission_classes = [permissions.IsAuthenticated]  
+    
+# class ModuleListEditview(generics.UpdateAPIView):
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def put(self, request, pk, format=None):
+#         queryset  = self.get_object(pk)
+#         serializer = ModuleListSerializer(queryset, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)     
+
+
+
+
+
+    # def post(self, request, format=None):
+    #     if  ModuleList.objects.filter(**request.data).exists():
+    #         raise serializers.ValidationError('This is Already Exists')
+        
+
+        
+    #     self.createdBy=self.request.user.username,
+    #     self.createdDate = date.today(), 
+    #     self.lastMoodifiedBy = self.request.user.username,
+    #     self.lastMoodifiedDate=date.today()
+    #     self.save()     
+
+# class ModuleListDetailsview(generics.ListCreateAPIView): 
+
+#     queryset = ModuleList.objects.all() 
+#     serializer_class = ModuleListSerializer   
+        
+
+
+# class ModuleListUpdationView(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = ModuleListSerializer
+#     queryset = ModuleList.objects.all()
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def perform_update(self, serializer):
+#         instance = self.get_object()  
+#         common_instance = instance.commonId
+#         datas = ModuleList.objects.get(id=common_instance)
+#         common_instance = CommonFields.objects.get(id=datas.commonId)
+        
+
+#         common_instance.lastModifiedBy = self.request.user.username
+#         common_instance.lastModifiedDate = date.today()
+#         common_instance.save()
+
+#         serializer.save()        
+
+
+
+
+# class ModuleListUpdationView(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = ModuleListSerializer
+#     queryset = ModuleList.objects.all() 
+#     print(queryset)
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def perform_create(self, serializer):
+#         instance_id = serializer.id
+#         print(instance_id)
+#         datas = ModuleList.objects.get(id=instance_id)
+     
+#         common_instance  = CommonFields.objects.get(id = datas.commonId )
+        
+#         common_instance.lastMoodifiedBy = self.request.user.username
+#         common_instance.lastMoodifiedDate=date.today()
+#         common_instance.save()
+        # serializer.save(commonId=common_instance)   
+
+
+
+# class moduleListView(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = ModuleListSerializer
+#     permission_classes = [permissions.AllowAny]
+
+#     def post(self, request, format=None):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data.get('user', None)
+
+#         if user is not None and user.is_active:
+#             common = CommonFields(createdBy=request.user.username)
+           
+#             return Response({'detail': 'Module created successfully'}, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+ 
+    
+
+               
+
+ 
